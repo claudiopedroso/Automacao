ac09a59933b49d91f83d39ef74dbabeef5367f11
Claudio Pedroso <claudiopedroso@gmail.com>
2017-08-02 11:50:03 -0300

    nokogiri instalado

--- testes_web/features/support/env.rb
+++ testes_web/features/support/env.rb
@@ -2,6 +2,8 @@ require 'capybara/cucumber'
 require 'capybara'
 require 'capybara/dsl'
 require 'selenium-webdriver'
+require 'nokogiri'
+require 'open-uri'
 
 Capybara.default_driver = :selenium
 Selenium::WebDriver::Firefox::Binary.path = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
